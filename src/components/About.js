402771fb10918b97283929ea8536641eda5d9131
Kenneth Fisher <kenandcrys@live.com>
2023-08-17 09:41:56 -0500

    udpate app.css and about background.

--- src/components/About.js
+++ src/components/About.js
@@ -4,10 +4,12 @@ import React from 'react';
 const About = () => {
     return (
         <div className="about">
-            <h4>Debbie Esterlein, a 52-year-old mother, finds immense joy in capturing the perfect moment through the lens of her camera. Being able to create lasting memories for families fills her with a wonderful feeling, and she considers herself an artist with a passion for photography.
+        <div>
+            <h1>Debbie Esterlein, a 52-year-old mother, finds immense joy in capturing the perfect moment through the lens of her camera. Being able to create lasting memories for families fills her with a wonderful feeling, and she considers herself an artist with a passion for photography.
    Despite not being able to afford college, Debbie has channeled her artistic skills into photography over the past two decades. With every photoshoot, she diligently hones her editing skills to deliver even better results for her clients.
   Debbie's upbringing, where her family couldn't afford frequent family pictures, has instilled in her a sense of empathy and understanding. In order to make sure families from all walks of life can afford the precious memories she creates, she prices her photoshoots reasonably.
-  Photography remains her beloved hobby, and she is grateful to be able to offer her services at affordable rates to help families cherish moments that will last a lifetime. Overall, Debbie Esterlein is a talented and compassionate photographer who uses her skills to bring joy to others and create cherished memories for families to treasure forever.</h4>
+  Photography remains her beloved hobby, and she is grateful to be able to offer her services at affordable rates to help families cherish moments that will last a lifetime. Overall, Debbie Esterlein is a talented and compassionate photographer who uses her skills to bring joy to others and create cherished memories for families to treasure forever.</h1>
+          </div>
         </div>
     )
 }
