3af13d0b6719a6af9e00b260cdb97c5c6f408dff
Chidi Tuke <c2kproductions@gmail.com>
2023-09-03 22:10:12 -0400

    added animation to slide

--- src/components/ImageSlider.js
+++ src/components/ImageSlider.js
@@ -1,10 +1,13 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { HomeImages } from "./HomeImagesData";
 import { FaArrowCircleLeft, FaArrowCircleRight } from "react-icons/fa";
 
 const ImageSlider = ({ img }) => {
   const [currentImg, setCurrentImg] = useState(0);
   const length = img.length;
+
+  const [userInteracted, setUserInteracted] = useState(false);
+  const slideTimeout = useRef(null);
 
   const nextSlide = () => {
     setCurrentImg(currentImg === length - 1 ? 0 : currentImg + 1);
@@ -14,14 +17,39 @@ const ImageSlider = ({ img }) => {
     setCurrentImg(currentImg === 0 ? length - 1 : currentImg - 1);
   };
 
+  useEffect(() => {
+    slideTimeout.current = userInteracted ? null : setTimeout(nextSlide, 4000);
+
+    return () => {
+      clearTimeout(slideTimeout.current);
+    };
+  }, [currentImg, userInteracted]);
+
+  const stopAutoSlide = () => {
+    setUserInteracted(true);
+    clearTimeout(slideTimeout.current);
+  };
+
   if (!Array.isArray(img) || img.length <= 0) {
     return null;
   }
 
   return (
     <section className="slider">
-      <FaArrowCircleLeft className="left-arrow" onClick={prevSlide} />
-      <FaArrowCircleRight className="right-arrow" onClick={nextSlide} />
+      <FaArrowCircleLeft
+        className="left-arrow"
+        onClick={() => {
+          stopAutoSlide();
+          prevSlide();
+        }}
+      />
+      <FaArrowCircleRight
+        className="right-arrow"
+        onClick={() => {
+          stopAutoSlide();
+          nextSlide();
+        }}
+      />
       {HomeImages.map((img, index) => {
         return (
           <div
