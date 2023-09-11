7af0831a19d0f7442c9205a3d4356930dea27a4d
Kenneth Fisher <kenandcrys@live.com>
2023-09-11 05:51:15 -0500

    Contact mdification

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,8 +2,20 @@ import React from 'react';
 
 const Contact = () => {
     return (
-        <div className="contact">
-            <h1>This is the Contact Page</h1>
+        <div className="contact-card">
+            <div>
+                
+            </div>
+            <div className='contact'>
+                <h1 style={{textAlign: "center"}} >Debbie Esterlein</h1>
+                <h3 style={{textAlign: "center"}} >Phone:&nbsp; 309.373.0952</h3>
+                <h3 style={{textAlign: "center"}}  >Email:&nbsp; <a href='mailto:debbie-nbp@gmail.com'>debbie-nbp@gmail.com</a></h3>
+                <h3 style={{textAlign: "center"}}  >Cash App:&nbsp; $DebraEsterlein</h3>
+                <h3 style={{textAlign: "center"}}  >Venmo: @debesterlein</h3>
+            </div>
+            <div>
+
+            </div>
         </div>
     )
 }
