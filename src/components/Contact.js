0bc999237313abbfed3a8d0639433881ec164e0d
Kenneth Fisher <kenandcrys@live.com>
2023-10-01 12:59:19 -0500

    update contact info

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import image from '../images/tammy-text.png';
+import '../App.css';
 
 const Contact = () => {
     return (
@@ -9,8 +10,8 @@ const Contact = () => {
             </div>
             <div className='contact'>
                 <h1 style={{textAlign: "center"}} >Debbie Esterlein</h1>
-                <h3 style={{textAlign: "center"}} >Phone:&nbsp;<a href="tel: 13093730952">309-373-0952</a></h3>
-                <h3 style={{textAlign: "center"}}  >Email:&nbsp; <a href='mailto:debbie-nbp@gmail.com'>debbie-nbp@gmail.com</a></h3>
+                <h3 style={{ textAlign: "center"}}>Phone:&nbsp;<a style={{textDecoration: "none"}} href="tel:13093730952">309-269-6665</a></h3>
+                <h3 style={{textAlign: "center"}}>Email:&nbsp; <a style={{textDecoration: 'none'}} href='mailto:debbie-nbp@gmail.com'>debbie-nbp@gmail.com</a></h3>
                 <h3 style={{textAlign: "center"}}  >Cash App:&nbsp; $DebraEsterlein</h3>
                 <h3 style={{textAlign: "center"}}  >Venmo: @debesterlein</h3>
             </div>
